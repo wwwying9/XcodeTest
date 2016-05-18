3f08d0657b1949a48bf072a3a629ff6cec59534a
姚英 <yaoying@yaoyingdemac-mini.local>
2016-05-18 17:19:52 +0800

    初始化工程

---
+++ XodeTese/XodeTese/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  XodeTese
+//
+//  Created by 姚英 on 16/5/18.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
